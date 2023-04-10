af38224087b11ab33072ea736ac875a91f23ef79
Richard Stacey <staceyrichard1996@gmail.com>
2023-04-10 17:25:49 -0600

    Fetch Working, unable to render list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+function BaseList() {
+
+const [amiibos, setAmiibos] = useState([])
+
+  useEffect(() => {
+    async function getAmiibos() {
+      const res = await fetch('https://www.amiiboapi.com/api/amiibo/')
+      const data = await res.json()
+
+      setAmiibos(data.amiibo)
+
+      console.log(data.amiibo)
+
+      const list = amiibos.map((figure) => {
+        <li>{figure.name}</li>
+      })
+
+      return (
+        <>
+          <ul>{list}</ul>
+        </>
+      )
+    }
+    getAmiibos()
+  }, [])
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h1>Amiibo!</h1>
+    <BaseList />
+    </>
+  )
 }
 
 export default App;
