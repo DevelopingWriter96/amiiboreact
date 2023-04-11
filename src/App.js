e79bff66acda8790612bde62a30fb703daefccc1
Richard Stacey <staceyrichard1996@gmail.com>
2023-04-11 11:02:17 -0600

    Retrieving Data, Banjo Kazooie Broken

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 function App() {
 
   const [amiibos, setAmiibos] = useState([])
+  const [selected, setSelected] = useState([])
 
   useEffect(() => {
     async function getAmiibos() {
@@ -18,14 +19,33 @@ function App() {
     getAmiibos()
   }, [])
 
+  async function details(name, series) {
+    const res = await fetch(`https://www.amiiboapi.com/api/amiibo/?amiiboSeries=${series}&name=${name}&showgames&showusage`)
+    const data = await res.json()
+    
+    console.log(data.amiibo[0])
+
+    setSelected(data.amiibo[0])
+
+    
+  }
+
   const list = amiibos.map((figure) => (
-    <li key={figure.tail}><img src={figure.image} class="icon"/>{figure.name}</li>
+    <li key={figure.tail} onClick={() => details(figure.name, figure.amiiboSeries)} ><img src={figure.image} class="icon" alt={figure.name}/>{figure.name}</li>
   ))
 
   return (
     <>
+    <div class="info">
+    <img src={selected.image} alt="selected amiibo"/>
+    <h2>{selected.name}</h2>
+    <h3>{selected.amiiboSeries}</h3>
+    <h3>{selected.gameSeries}</h3>
+    </div>
+    <div class="list">
     <h1>Amiibo!</h1>
     <ul>{list}</ul>
+    </div>
     </>
   )
 }
