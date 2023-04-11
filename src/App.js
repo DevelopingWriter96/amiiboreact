fd569919bd1862e6f16dcefe14cc6c59b872ab6f
Richard Stacey <staceyrichard1996@gmail.com>
2023-04-11 14:25:56 -0600

    Glitch fixed

--- src/App.js
+++ src/App.js
@@ -19,19 +19,68 @@ function App() {
     getAmiibos()
   }, [])
 
-  async function details(name, series) {
-    const res = await fetch(`https://www.amiiboapi.com/api/amiibo/?amiiboSeries=${series}&name=${name}&showgames&showusage`)
-    const data = await res.json()
-    
-    console.log(data.amiibo[0])
+  async function details(name, series, type) {
+    if (name === "Banjo & Kazooie") {
+      const res = await fetch('https://www.amiiboapi.com/api/amiibo/?character=Banjo&showgames&showusage')
+      const data = await res.json()
 
-    setSelected(data.amiibo[0])
+      console.log(data.amiibo[0])
 
-    
+      setSelected(data.amiibo[0])
+    } else if (name === "Midna & Wolf Link") {
+      const res = await fetch('https://www.amiiboapi.com/api/amiibo/?character=Midna&showgames&showusage')
+      const data = await res.json()
+
+      console.log(data.amiibo[0])
+
+      setSelected(data.amiibo[0])
+    } else if(name==="Zelda & Loftwing") {
+      const res = await fetch('https://www.amiiboapi.com/api/amiibo/?character=Zelda&showgames&showusage')
+      const data = await res.json()
+
+      console.log(data.amiibo[4])
+
+      setSelected(data.amiibo[4])
+    } else if(name==="Rosalina & Luma") {
+      const res = await fetch('https://www.amiiboapi.com/api/amiibo/?character=Rosalina&showgames&showusage')
+      const data = await res.json()
+
+      console.log(data.amiibo[1])
+
+      setSelected(data.amiibo[1])
+    } else if(name==="Timmy & Tommy" && type==="Card") {
+      const res = await fetch('https://www.amiiboapi.com/api/amiibo/?gameseries=Animal Crossing&showgames&showusage')
+      const data = await res.json()
+
+      console.log(data.amiibo[22])
+      
+      setSelected(data.amiibo[22])
+    } else if(name==="Timmy & Tommy" && type==="Figure") {
+      const res = await fetch('https://www.amiiboapi.com/api/amiibo/?gameseries=Animal Crossing&showgames&showusage')
+      const data = await res.json()
+
+      console.log(data.amiibo[21])
+      
+      setSelected(data.amiibo[21])
+    } else if(name==="Mr. Game & Watch") {
+      const res = await fetch('https://www.amiiboapi.com/api/amiibo/?amiiboseries=Super Smash Bros.&showgames&showusage')
+      const data = await res.json()
+
+      console.log(data.amiibo[590])
+      
+      setSelected(data.amiibo[590])
+    } else {  
+      const res = await fetch(`https://www.amiiboapi.com/api/amiibo/?amiiboSeries=${series}&name=${name}&type=${type}&showgames&showusage`)
+      const data = await res.json()
+      
+      console.log(data.amiibo[0])
+
+      setSelected(data.amiibo[0])
+    }
   }
 
   const list = amiibos.map((figure) => (
-    <li key={figure.tail} onClick={() => details(figure.name, figure.amiiboSeries)} ><img src={figure.image} class="icon" alt={figure.name}/>{figure.name}</li>
+    <li key={figure.tail} onClick={() => details(figure.name, figure.amiiboSeries, figure.type)} ><img src={figure.image} class="icon" alt={figure.name}/>{figure.name}</li>
   ))
 
   return (
