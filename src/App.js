dd847e78cd9352bfa016d6868fe2480219b03225
Richard Stacey <staceyrichard1996@gmail.com>
2023-04-12 14:42:31 -0600

    Game initial game retrieve working

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
 
   const [amiibos, setAmiibos] = useState([])
   const [selected, setSelected] = useState([])
+  const [games3ds, setGames3ds] = useState([])
+  const [gamesWiiu, setGamesWiiu] = useState([])
+  const [gamesSwitch, setGamesSwitch] = useState([])
+  const [date, setDate] = useState("0000-00-00")
 
+  //Amiibo List Retrieval
   useEffect(() => {
     async function getAmiibos() {
       const res = await fetch('https://www.amiiboapi.com/api/amiibo/')
@@ -19,6 +24,7 @@ function App() {
     getAmiibos()
   }, [])
 
+  //Amiibo Details Retrieval
   async function details(name, series, type) {
     if (name === "Banjo & Kazooie") {
       const res = await fetch('https://www.amiiboapi.com/api/amiibo/?character=Banjo&showgames&showusage')
@@ -48,6 +54,7 @@ function App() {
       console.log(data.amiibo[1])
 
       setSelected(data.amiibo[1])
+
     } else if(name==="Timmy & Tommy" && type==="Card") {
       const res = await fetch('https://www.amiiboapi.com/api/amiibo/?gameseries=Animal Crossing&showgames&showusage')
       const data = await res.json()
@@ -77,7 +84,29 @@ function App() {
 
       setSelected(data.amiibo[0])
     }
+
+    console.log(selected.release.na)
+
+    setDate(selected.release.na)
+
+    setGames3ds(selected.games3DS)
+
+    setGamesWiiu(selected.gamesWiiU)
+
+    setGamesSwitch(selected.gamesSwitch)
   }
+
+  const ds = games3ds.map((game) => (
+    <li key={game.gameID[0]}>{game.gameName}</li>
+  ))
+
+  const wiiu = gamesWiiu.map((game) => (
+    <li key={game.gameID[0]}>{game.gameName}</li>
+  ))
+  
+  const ns = gamesSwitch.map((game) => (
+    <li key={game.gameID[0]}>{game.gameName}</li>
+  ))
 
   const list = amiibos.map((figure) => (
     <li key={figure.tail} onClick={() => details(figure.name, figure.amiiboSeries, figure.type)} ><img src={figure.image} class="icon" alt={figure.name}/>{figure.name}</li>
@@ -85,14 +114,21 @@ function App() {
 
   return (
     <>
+    <h1>Amiibo!</h1>
     <div class="info">
-    <img src={selected.image} alt="selected amiibo"/>
+    <img src={selected.image} alt="selected amiibo" class="selectedImage"/>
     <h2>{selected.name}</h2>
-    <h3>{selected.amiiboSeries}</h3>
-    <h3>{selected.gameSeries}</h3>
+    <h3>Released in the US on {date} as part of the {selected.amiiboSeries} Amiibo Series</h3>
+    <h3>Character is from {selected.gameSeries}</h3>
+    <h3>Compatible Games:</h3>
+    <h4>3DS</h4>
+    <ul>{ds}</ul>
+    <h4>WiiU</h4>
+    <ul>{wiiu}</ul>
+    <h4>Switch</h4>
+    <ul>{ns}</ul>
     </div>
     <div class="list">
-    <h1>Amiibo!</h1>
     <ul>{list}</ul>
     </div>
     </>
